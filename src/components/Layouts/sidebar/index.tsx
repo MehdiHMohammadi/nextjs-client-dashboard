c8522d6cc1343089fd3673d7dd455c971c586e61
MehdiHMohammadi <mehdihmohammadi@gmail.com>
2025-08-18 14:39:37 +0400

    edit sidbar

--- src/components/Layouts/sidebar/index.tsx
+++ src/components/Layouts/sidebar/index.tsx
@@ -1,12 +1,10 @@
-"use client";
-
 import { Logo } from "@/components/logo";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import { NAV_DATA } from "./data";
-import { ArrowLeftIcon, ChevronUp } from "./icons";
+import { ArrowLeftIcon } from "./icons";
 import { MenuItem } from "./menu-item";
 import { useSidebarContext } from "./sidebar-context";
 
@@ -15,27 +13,17 @@ export function Sidebar() {
   const { setIsOpen, isOpen, isMobile, toggleSidebar } = useSidebarContext();
   const [expandedItems, setExpandedItems] = useState<string[]>([]);
 
-  const toggleExpanded = (title: string) => {
-    setExpandedItems((prev) => (prev.includes(title) ? [] : [title]));
-
-    // Uncomment the following line to enable multiple expanded items
-    // setExpandedItems((prev) =>
-    //   prev.includes(title) ? prev.filter((t) => t !== title) : [...prev, title],
-    // );
-  };
-
- useEffect(() => {
-  const newExpandedItems: string[] = [];
-  NAV_DATA.forEach((section) => {
-    section.items.forEach((item) => {
-      if (item.url === pathname) {
-        newExpandedItems.push(item.title);
-      }
+  useEffect(() => {
+    const newExpandedItems: string[] = [];
+    NAV_DATA.forEach((section) => {
+      section.items.forEach((item) => {
+        if (item.url === pathname) {
+          newExpandedItems.push(item.title);
+        }
+      });
     });
-  });
-  setExpandedItems(newExpandedItems);
-}, [pathname]);
-
+    setExpandedItems(newExpandedItems);
+  }, [pathname]);
 
   return (
     <>
@@ -52,11 +40,10 @@ export function Sidebar() {
         className={cn(
           "max-w-[290px] overflow-hidden border-r border-gray-200 bg-white transition-[width] duration-200 ease-linear dark:border-gray-800 dark:bg-gray-dark",
           isMobile ? "fixed bottom-0 top-0 z-50" : "sticky top-0 h-screen",
-          isOpen ? "w-full" : "w-0",
+          isOpen ? "w-full" : "w-0"
         )}
         aria-label="Main navigation"
         aria-hidden={!isOpen}
-        // inert={!isOpen}
         inert={!isOpen ? true : undefined}
       >
         <div className="flex h-full flex-col py-10 pl-[25px] pr-[7px]">
@@ -75,8 +62,7 @@ export function Sidebar() {
                 className="absolute left-3/4 right-4.5 top-1/2 -translate-y-1/2 text-right"
               >
                 <span className="sr-only">Close Menu</span>
-
-                <ArrowLeftIcon className="ml-auto size-7" />
+                <ArrowLeftIcon className="mr-auto size-7" />
               </button>
             )}
           </div>
@@ -93,76 +79,20 @@ export function Sidebar() {
                   <ul className="space-y-2">
                     {section.items.map((item) => (
                       <li key={item.title}>
-                        {item.items.length ? (
-                          <div>
-                            <MenuItem
-                              isActive={item.items.some(
-                                ({ url }) => url === pathname,
-                              )}
-                              onClick={() => toggleExpanded(item.title)}
-                            >
-                              <item.icon
-                                className="size-6 shrink-0"
-                                aria-hidden="true"
-                              />
-
-                              <span>{item.title}</span>
-
-                              <ChevronUp
-                                className={cn(
-                                  "mr-auto rotate-180 transition-transform duration-200",
-                                  expandedItems.includes(item.title) &&
-                                    "rotate-0",
-                                )}
-                                aria-hidden="true"
-                              />
-                            </MenuItem>
-
-                            {expandedItems.includes(item.title) && (
-                              <ul
-                                className="ml-9 mr-0 space-y-1.5 pb-[15px] pr-0 pt-2"
-                                role="menu"
-                              >
-                                {/* {item.items.map((subItem) => (
-                                  <li key={subItem.title} role="none">
-                                    <MenuItem
-                                      as="link"
-                                      href={subItem.url}
-                                      isActive={pathname === subItem.url}
-                                    >
-                                      <span>{subItem.title}</span>
-                                    </MenuItem>
-                                  </li>
-                                ))} */}
-                              </ul>
-                            )}
-                          </div>
-                        ) : (
-                          // (() => {
-                          //   const href =
-                          //     "url" in item
-                          //       ? item.url + ""
-                          //       : "/" +
-                          //         item.title.toLowerCase().split(" ").join("-");
-
-                          //   return (
-                          //     <MenuItem
-                          //       className="flex items-center gap-3 py-3"
-                          //       as="link"
-                          //       href={href}
-                          //       isActive={pathname === href}
-                          //     >
-                          //       <item.icon
-                          //         className="size-6 shrink-0"
-                          //         aria-hidden="true"
-                          //       />
-
-                          //       <span>{item.title}</span>
-                          //     </MenuItem>
-                          //   );
-                          // })()
-                          <h1>Menu</h1>
-                        )}
+                        <MenuItem
+                          className="flex items-center gap-3 py-3"
+                          as="link"
+                          href={item.url}
+                          isActive={pathname === item.url}
+                        >
+                          {item.icon && (
+                            <item.icon
+                              className="size-6 shrink-0"
+                              aria-hidden="true"
+                            />
+                          )}
+                          <span>{item.title}</span>
+                        </MenuItem>
                       </li>
                     ))}
                   </ul>
